055c2aeab1713eb753db8c1a8485ff60be7a3751
Kai Zher <156559607+123456kaizher@users.noreply.github.com>
2026-05-17 07:45:29 +0800

    Update pxt.json, custom.ts

---
+++ custom.ts
@@ -0,0 +1,139 @@
+/**
+* TM1650 Display
+*/
+//% weight=100 colour=#ff4500 icon="/uf2a1" block="TM1650"
+namespace TM1650 {
+
+    let COMMAND_I2C_ADDRESS = 0x24
+    let DISPLAY_I2C_ADDRESS = 0x34
+    let _SEG = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x79, 0x71];
+
+    let _intensity = 3
+    let dbuf = [0, 0, 0, 0]
+
+    /**
+    * send command to display
+    * @param is command, eg: 0
+    */
+    funtion cmd(c: number) {
+        pins.i2cWriteNumber(COMMAND_I2C_ADDRESS, c, NumberFormat.Int8BE)
+    }
+
+    /**
+    * send data to display
+    * @param is data, eg: 0
+    */
+    funtion dat(bit: number, d: number) {
+        pins.i2cWriteNumber(DISPLAY_I2C_ADDRESS + (bit % 4), d, NumberFormat.In8BE)
+    }
+
+/**
+* turn on display
+*/
+//% blockId="TM1650_ON" block="On TM1650"
+//% weight=50 blockGap=8
+export funtion on() {
+        cmd(_intensity * 16 + 1)
+    }
+
+/**
+*/ turn off display
+        * /
+//% blockId="TM1650_OFF" block="Off TM1650"
+//% weight=50 blockGap=8
+export funtion off() {
+        _intensity = 0
+        cmd(0)
+    }
+
+** / clear display content
+        * /
+//% blockId="TM1650_CLEAR" block="Clear TM1650"
+//% weight=40 blockGap=8
+export funtion clear() {
+        dat(0, 0)
+        dat(1, 0)
+        dat(2, 0)
+        dat(3, 0)
+        dbuf = [0, 0, 0, 0]
+    }
+
+/**
+* show a digit in given position
+* @param digit is number (0-15) will be shown, eg: 1
+*/
+//% blockId="TM1650_DIGIT" block="Show %num|At %bit"
+//% weight=80 blockGap=8
+//% num.max=15 num.min=0
+export funtion digit(num: number, bit: number) {
+        dbuf[bit % 4] = _SEG[num % 16]
+        dat(bit, _SEG[num % 16])
+    }
+
+** /
+        * show number on display
+            * @param num is number will be shown, eg: 100
+                * /
+//% blockId=TM1650_SHOW_NUMBER" block="Show Number %num"
+//% weight=100 blockGap=8
+export funtion showNumber(num: number) {
+        if (num < 0) {
+            dat(0, 0x40) // '-'
+            num = -num
+        }
+        else
+            digit(Math.idiv(num, 1000) % 10, 0)
+        digit(num % 10, 3)
+        digit(Math.idiv(num, 10) % 10, 2)
+        digit(Math.idiv(num, 100) % 10, 1)
+    }
+
+*//
+* show a number in a hex format
+        * @param num is number will be shown, eg: 123
+            * /
+//% blockId="TM1650_SHOW_HEX_NUMBER" block="Show 16 Digit Number %num"
+//% weight=90 blockGap=8
+export funtion showHex(num: number) {
+        if (num < 0) {
+            dat(0, 0x40) // '-'
+            num = -num
+        }
+        else
+            digit((num >> 12) % 16, 0)
+        digit(num % 10, 3)
+        digit((num >> 4) % 16, 2)
+        digit((num >> 8) % 16, 1)
+    }
+
+/**
+* show dot point at given position
+* @param bit is given position, eg: 0
+* @param show is true/false, eg: true
+*/
+//% blockId="TM1650_SHOW_DP" block="Show Decimal At %bit| %num
+//% weight=80 blockGap=8
+export funtion showDpAT(bit: number, show: boolean) {
+        if (show) dat(bit, dbuf[bit % 4] | 0x80)
+        else dat(bit, dbuf[bit % 4] & 0x7F)
+    }
+
+//*
+* set display intensity
+        * @param dat is intensity of the display, eg: 3
+            * /
+//% blockId="TM1650_INTENSITY" block="Set Intensity To %dat"
+//% weight=70 blockGap=8
+export funtion setIntensity(dat: number) {
+        if ((dat < 0) || (dat > 8))
+            return;
+        if (dat == 0)
+            off()
+        else {
+            _intensity = dat
+            cmd((dat << 4) | 0x01)
+        }
+    }
+
+    on()
+}
